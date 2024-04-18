ff1a948072cacf5c79270b5c59a4449c76f86cd2
pariitosh <paritoshsagar25@gmail.com>
2024-04-18 18:07:56 +0530

    changed to images

--- src/Portal1.jsx
+++ src/Portal1.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { Canvas } from '@react-three/fiber'
-import { PerspectiveCamera,CameraControls, Environment, Html, Image, Circle } from '@react-three/drei'
+import { PerspectiveCamera,CameraControls, Environment, Html, Image, Circle, Box } from '@react-three/drei'
 import { useRef, useState } from 'react'
 import { Bar } from './Bar'
 import gsap from 'gsap'
@@ -14,7 +14,7 @@ export default function Portal1(){
     const enter=()=>{
         gsap.to(camera.current.position,{z:-20})
         gsap.to(camera.current.position,{y:-5})
-        gsap.to(camera.current.position,{x:35})
+        gsap.to(camera.current.position,{x:40})
         setCurrentPhase("pickScreen")
     }
     const openbottle=()=>{
@@ -54,7 +54,7 @@ export default function Portal1(){
     }
 
     const screen1=()=>{
-        gsap.to(camera.current.position,{x:35})
+        gsap.to(camera.current.position,{x:40})
         gsap.to('.blackline',{width:"0%"})
     }
     const screen2=()=>{
@@ -62,7 +62,7 @@ export default function Portal1(){
         gsap.to('.blackline',{width:"50%"})
     }
     const screen3=()=>{
-        gsap.to(camera.current.position,{x:-35})
+        gsap.to(camera.current.position,{x:-40})
         gsap.to('.blackline',{width:"100%"})
     }
     
@@ -113,17 +113,49 @@ export default function Portal1(){
             <Canvas style={{background:'linear-gradient(90deg, rgba(245,182,168,1) 0%, rgba(241,121,96,1) 52%, rgba(249,213,206,1) 100%)'}}   >
                 <PerspectiveCamera   ref={camera} position={[0,-7,-45]} rotation={[0,0,0]}>
                     <CameraControls  />
-                    <Bar rotation={[0,0*(Math.PI/180),0]} scale={[0.20,0.20,0.20]}/>
-                    <Bar position={[35,0,0]} rotation={[0,0*(Math.PI/180),0]} scale={[0.20,0.20,0.20]}/>
-                    <Bar position={[-35,0,0]} rotation={[0,0*(Math.PI/180),0]} scale={[0.20,0.20,0.20]}/>
-                    <Image onClick={openbottle} scale={[3,3,3]} position={[-35,8,10]}  url='/circle.png' ></Image>
-                    <Image onClick={openplaylist} scale={[3,3,3]} position={[-40,6,10]}  url='/circle.png' ></Image>
-                    <Image onClick={openvideo} scale={[3,3,3]} position={[-32,4,10]}  url='/circle.png' ></Image>
-                        
-                    <Circle></Circle>
+                    
+                    
+                    
+                    <Image scale={[15,13,13]} position={[-40,5,10]} url='/world 1.png'></Image>
+                    <Image scale={[15,13,13]} position={[0,5,10]} url='/world 2.png'></Image>
+                    <Image scale={[15,13,13]} position={[40,5,10]} url='/world 3.png'></Image>
+                    
+                    <TouchPoint clicked={openbottle} position={[-40,8,11]}/>
+                    <TouchPoint clicked={openvideo} position={[-36,2,11]}/>
+                    <TouchPoint clicked={openplaylist} position={[-46,4,11]}/>
+
+                    <TouchPoint clicked={openbottle} position={[-0,8,11]}/>
+                    <TouchPoint clicked={openvideo} position={[4,5,10]}/>
+                    <TouchPoint clicked={openplaylist} position={[-4,4,11]}/>
+
+                    <TouchPoint clicked={openbottle} position={[40,8,11]}/>
+                    <TouchPoint clicked={openvideo} position={[44,5,10]}/>
+                    <TouchPoint clicked={openplaylist} position={[36,4,11]}/>
+
+
                 </PerspectiveCamera>
                 <Environment preset='city'/>
             </Canvas>
         </div>
     )
 }
+
+const TouchPoint=({position,clicked})=>{
+    return (
+    <group onClick={clicked}>
+        <mesh visible position={position} rotation={[0, 0, 0]} castShadow scale={[0.3,0.3,0.3]}>
+            <ringGeometry   args={[3.8, 4, 28]} />
+            <meshBasicMaterial attach="material" color="black" />
+        </mesh>
+        <Circle material-color={'black'} scale={[0.2,0.2,0.2]} position={position}></Circle>
+        <Circle visible={false} scale={[1.2,1.2,1.2]} material-color={'black'} position={position}></Circle>
+    </group>
+    )
+}
+/*
+
+        <Bar rotation={[0,0*(Math.PI/180),0]} scale={[0.20,0.20,0.20]}/>
+        <Bar position={[35,0,0]} rotation={[0,0*(Math.PI/180),0]} scale={[0.20,0.20,0.20]}/>
+        <Bar position={[-35,0,0]} rotation={[0,0*(Math.PI/180),0]} scale={[0.20,0.20,0.20]}/>
+
+*/
